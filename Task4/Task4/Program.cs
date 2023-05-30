d81912a36dfae7a4e53bf420f8b3620c3352bebb
muradraliyev <133110003+muradraliyev@users.noreply.github.com>
2023-05-30 13:13:27 +0400

    Task4

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers1 = { 4, 5, 2, 6, 4, 3 };
+            int[] numbers2 = { 7, 4, 2, 76, 45, 65 };
+
+            Difference(numbers1, numbers2);
+        }
+
+        static void Difference(int[] arr1, int[] arr2)
+        {
+            foreach (int i in arr1)
+            {
+                int a = 0;
+
+                foreach (int j in arr2)
+                {
+                    
+                    if (i == j)
+                    {
+                        a++;
+                    }
+                    
+                }
+                if (a == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        } } }
