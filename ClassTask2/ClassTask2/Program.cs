b2223b092bdb46dcbc8d834695d6a688e54d5373
muradraliyev <133110003+muradraliyev@users.noreply.github.com>
2023-05-30 12:56:03 +0400

    Task2

---
+++ ClassTask2/ClassTask2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ClassTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user_name="";
+            Console.WriteLine("Username daxil edin:");
+
+            while (user_name.Length < 8)
+            {
+                user_name = Console.ReadLine();
+                if (user_name.Length < 8)
+                {
+                    Console.WriteLine("Duzgun Username daxil edin:");
+                }
+            }
+
+            User user = new User(user_name);
+
+
+
+            string pass_word = "";
+            Console.WriteLine("Sifre daxil edin:");
+
+            while (pass_word.Length < 8 || !pass_word.Any(char.IsDigit) || (pass_word.ToUpper().Equals(pass_word)) || (pass_word.ToLower().Equals(pass_word)))
+            {
+                pass_word = Console.ReadLine();
+                if (pass_word.Length < 8 || !pass_word.Any(char.IsDigit) || (pass_word.ToUpper().Equals(pass_word)) || (pass_word.ToLower().Equals(pass_word)))
+                {
+                    Console.WriteLine("Duzgun sifre daxil edin:");
+                }
+            }
+
+            user.Password = pass_word;
+
+
+            Console.WriteLine(user.Username);
+            Console.WriteLine(user.Password);
+        }
+
+
+    }
+}
---
+++ ClassTask2/ClassTask2/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask2
+{
+    internal class User
+    {
+        private string userName;
+        private string password;
+
+
+        public User(string name)
+        {
+            Username = name;
+        }
+
+        public string Username
+        {
+
+            get => userName;
+            set
+            {
+
+                if (value.Length >= 8)
+                {
+                    userName = value;
+                }
+                else 
+                { 
+                    Console.WriteLine("Duzgun daxil edin! 8 simvoldan cox olmalidir");
+                }
+
+            }
+
+        }
+        public string Password
+        {
+
+            get => password;
+            set
+            {
+
+                if (value.Length>=8 && value.Any(char.IsDigit) && !(value.ToUpper().Equals(value)) && !(value.ToLower().Equals(value)))
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Sifrenin uzunlugu minimum 8 olmalidir ve icinde en az 1 boyuk herf,1 kicik herf ve 1 reqem olmalidir.");
+                }
+
+            }
+
+        }
+    }
+}
